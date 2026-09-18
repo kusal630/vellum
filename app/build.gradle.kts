6a3b2ea6a7bf038511df49dcb9270647105a9323
Venkata Naga Kusal <kottekvnkusal@gmail.com>
2026-09-18 21:23:05 +0530

    chore(release): bump to 1.7.0 (versionCode 8)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,8 +18,8 @@ android {
         applicationId = "com.vellum.notes"
         minSdk = 26
         targetSdk = 35
-        versionCode = 7
-        versionName = "1.6.0"
+        versionCode = 8
+        versionName = "1.7.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
